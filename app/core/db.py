8852a0f17f1acb169a266d7f38d9f074f1af768d
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-11-06 10:10:17 -0600

    Add render configurtions

--- app/core/db.py
+++ app/core/db.py
@@ -1,12 +1,19 @@
 
+import os
 from typing import Iterator
 from sqlmodel import SQLModel, Session, create_engine
 
 from app.core.config import settings
 
+DATABASE_URL = os.environ["DATABASE_URL"]
+connect_args = {}
+if DATABASE_URL.startswith("postgres"):
+    connect_args = {"sslmode": "require"}
+engine = create_engine(DATABASE_URL, pool_pre_ping=True,
+                       connect_args=connect_args)
 
-engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={
-                       "check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {})
+# engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={
+#                        "check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {})
 
 
 def init_db() -> None:
